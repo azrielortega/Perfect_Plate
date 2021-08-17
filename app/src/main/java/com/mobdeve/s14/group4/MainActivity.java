8ce97d68d773378d9928aaf51c6089e47251ca6c
Azriel <44496680+azrielortega@users.noreply.github.com>
2021-08-18 01:18:21 +0800

    main page, login design done!

--- app/src/main/java/com/mobdeve/s14/group4/MainActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/MainActivity.java
@@ -2,13 +2,31 @@ package com.mobdeve.s14.group4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.main_btn_login);
+        btnSignUp = findViewById(R.id.main_btn_join);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
