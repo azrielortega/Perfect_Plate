66dd7c4bbb11e0691a70d515a720230b111b50de
chezkacruzz <franchezka_cruz@dlsu.edu.ph>
2021-08-19 01:38:00 +0800

    add home view; next time download icons as svg
    - adjusted input fonts to montserrat

--- app/src/main/java/com/mobdeve/s14/group4/MainActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_home); DONT DELETE 
         setContentView(R.layout.activity_main);
 
         btnLogin = findViewById(R.id.main_btn_login);
