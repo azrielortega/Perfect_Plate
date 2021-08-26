f8d78703d7a2772426825a1a1642ce5cc45398f5
Azriel <44496680+azrielortega@users.noreply.github.com>
2021-08-26 20:50:44 +0800

    connected buttons and create recipe buttons

--- app/src/main/java/com/mobdeve/s14/group4/CreateRecipeActivity1.java
+++ app/src/main/java/com/mobdeve/s14/group4/CreateRecipeActivity1.java
@@ -1,14 +1,52 @@
 package com.mobdeve.s14.group4;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CreateRecipeActivity1 extends AppCompatActivity {
+
+    private Button btnCreate1;
+
+    private ImageButton ibBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe1);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        btnCreate1 = findViewById(R.id.btn_create1_next);
+        ibBack = findViewById(R.id.ib_create1_back);
+
+        ibBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnCreate1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CreateRecipeActivity1.this, CreateRecipeActivity2.class);
+                startActivityForResult(i, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        finish();
     }
 }
--- app/src/main/java/com/mobdeve/s14/group4/SearchActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/SearchActivity.java
@@ -2,13 +2,53 @@ package com.mobdeve.s14.group4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private LinearLayout llProfile;
+    private LinearLayout llHome;
+    private LinearLayout llCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        initComponents();
+
+
+    }
+
+    private void initComponents() {
+        this.llProfile = findViewById(R.id.ll_profile);
+        this.llHome = findViewById(R.id.ll_home);
+        llCreate = findViewById(R.id.ll_create);
+
+        this.llProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SearchActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        this.llHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        this.llCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SearchActivity.this, CreateRecipeActivity1.class);
+                startActivity(i);
+            }
+        });
     }
 }
