80053081e7246cdd4a49087bb66d296692c7569a
Azriel <44496680+azrielortega@users.noreply.github.com>
2021-08-27 13:16:47 +0800

    merge

--- app/src/main/java/com/mobdeve/s14/group4/MainActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 MainActivity.this.startActivity(intent);
             }
         });
