c07bebce2a79ad33968cb1a9ad526425f2a64cb7
chezkacruzz <franchezka_cruz@dlsu.edu.ph>
2021-08-27 15:34:01 +0800

    view recipe details fixed

--- app/src/main/java/com/mobdeve/s14/group4/RecipeDetailsActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/RecipeDetailsActivity.java
@@ -2,8 +2,10 @@ package com.mobdeve.s14.group4;
 
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,6 +23,12 @@ public class RecipeDetailsActivity extends AppCompatActivity {
     private TextView tvDescription;
     private TextView tvReviewCount;
     private LinearLayout llWriteReview;
+    private TextView tvIngredients;
+    private TextView tvInstructions;
+
+    private ConstraintLayout clIngredients;
+    private ConstraintLayout clInstructions;
+    private ConstraintLayout clReviews;
 
 
     @Override
@@ -39,6 +47,13 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         this.tvReviewCount = findViewById(R.id.tv_details_review_count);
 
         this.llWriteReview = findViewById(R.id.ll_write_review);
+        this.clIngredients = findViewById(R.id.cl_details_ingredients);
+        this.clInstructions = findViewById(R.id.cl_details_instructions);
+
+        this.tvIngredients = findViewById(R.id.tv_details_ingredients);
+        this.tvInstructions = findViewById(R.id.tv_details_instructions);
+
+        //this.clReviews = findViewById(R.id.cl_details_reviews);
 
         Intent i = getIntent();
 
@@ -67,11 +82,41 @@ public class RecipeDetailsActivity extends AppCompatActivity {
 //        int iRC = i.getIntExtra(PopularAdapter.KEY_RECIPE_REVIEWS_COUNT, 0);
 //        this.tvReviewCount.setText(iRC);
 
+        this.clIngredients.setVisibility(View.VISIBLE);
+
         this.llWriteReview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(), WriteReviewActivity.class);
                 startActivity(i);
+            }
+        });
+
+        this.tvIngredients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clIngredients.setVisibility(View.VISIBLE);
+                tvIngredients.setBackgroundResource(R.color.proj_yellow);
+                tvIngredients.setTextColor(getResources().getColor(R.color.proj_white));
+
+
+                clInstructions.setVisibility(View.GONE);
+                tvInstructions.setBackgroundColor(getResources().getColor(R.color.proj_white));
+                tvInstructions.setTextColor(getResources().getColor(R.color.proj_blue));
+
+            }
+        });
+
+        this.tvInstructions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clInstructions.setVisibility(View.VISIBLE);
+                tvInstructions.setBackgroundColor(getResources().getColor(R.color.proj_yellow));
+                tvInstructions.setTextColor(getResources().getColor(R.color.proj_white));
+
+                clIngredients.setVisibility(View.GONE);
+                tvIngredients.setBackgroundResource(R.color.proj_white);
+                tvIngredients.setTextColor(getResources().getColor(R.color.proj_blue));
             }
         });
 
