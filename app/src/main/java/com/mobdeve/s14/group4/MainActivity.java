a27aadd27eff0493b069ea10573e8da776f5b1de
tanal <tanal@DESKTOP-G2ICFLV.local>
2021-09-09 00:09:40 +0800

    comment out onstart

--- app/src/main/java/com/mobdeve/s14/group4/MainActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/MainActivity.java
@@ -47,15 +47,15 @@ public class MainActivity extends AppCompatActivity {
         this.mAuth = FirebaseAuth.getInstance();
     }
 
-    @Override
-    public void onStart(){
-        super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        if (currentUser != null){
-            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-            MainActivity.this.startActivity(intent);
-        }
-    }
+//    @Override
+//    public void onStart(){
+//        super.onStart();
+//
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//
+//        if (currentUser != null){
+//            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//            MainActivity.this.startActivity(intent);
+//        }
+//    }
 }
