f710e4a0c8462d8aaa77d76e8bc56b6b05fdcc6e
tanal <tanal@DESKTOP-G2ICFLV.local>
2021-09-15 01:41:06 +0800

    refactor home activity

--- app/src/main/java/com/mobdeve/s14/group4/HomeActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/HomeActivity.java
@@ -43,13 +43,6 @@ public class HomeActivity extends AppCompatActivity {
 
     private ImageView ivSearch;
 
-    private FirebaseDatabase database;
-    private DatabaseReference databaseReference;
-    private DatabaseReference ingredientDatabaseReference;
-
-    public static ArrayList<Recipe> recipeList;
-    private ArrayList<Ingredient> allIngredientList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,7 +51,7 @@ public class HomeActivity extends AppCompatActivity {
 
         this.initComponents(); // init components and load data
 
-        //this.initPopularRecyclerView();
+        this.initPopularRecyclerView();
         this.initRecentFeed();
     }
 
@@ -68,44 +61,6 @@ public class HomeActivity extends AppCompatActivity {
         llCreate = findViewById(R.id.ll_create);
         ivSearch = findViewById(R.id.iv_search);
         llFavorites = findViewById(R.id.ll_fav);
-
-        this.database = FirebaseDatabase.getInstance();
-        this.databaseReference = this.database.getReference("recipes");
-        this.ingredientDatabaseReference = this.database.getReference("ingredients");
-        recipeList = new ArrayList<>();
-        this.allIngredientList = new ArrayList<>();
-
-        Log.d("LISTSIZE", String.valueOf(recipeList.size()));
-
-
-
-        ingredientDatabaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                allIngredientList.clear();
-                for (DataSnapshot ingredientSnapshot : snapshot.getChildren()){
-                    String id = ingredientSnapshot.getKey();
-                    String ingredientName = ingredientSnapshot.child("ingredientName").getValue().toString();
-                    Double qty = Double.valueOf(ingredientSnapshot.child("quantity").getValue().toString());
-                    String units = ingredientSnapshot.child("units").getValue().toString();
-
-                    Log.d("INGREDIENT ID", id);
-                    Log.d("INGREDIENT NAME", ingredientName);
-                    Ingredient temp = new Ingredient(id, qty, units, ingredientName);
-                    allIngredientList.add(temp);
-                    initRecipes();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-                System.out.println("The read failed: " + error.getCode());
-            }
-        });
-
-
-
-        //initRecipes
 
         llFavorites.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -138,92 +93,10 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        Log.d("RECIPE LIST SIZE aaaaaa", String.valueOf(recipeList.size()));
     }
 
-    private void loadRecipes(){
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
-                Recipe recipes = dataSnapshot.getValue(Recipe.class);
-                Log.d("recipes", String.valueOf(recipes));
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
-    }
-
-    private void initRecipes(){
-        final Recipe[] tempRecipe = new Recipe[1];
-
-        databaseReference.addValueEventListener(new ValueEventListener() { // load all recipes
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
-                int ctr = 0;
-                recipeList.clear();
-                Recipe recipes = dataSnapshot.getValue(Recipe.class);
-                Log.d("meow", "meow");
-                Log.d("WAAAAHHHHH", String.valueOf(allIngredientList.size()));
-                Log.d("MOOOO", String.valueOf(recipeList.size()));
-
-                for (DataSnapshot recipeSnapshot : dataSnapshot.getChildren()){
-                    String id = recipeSnapshot.getKey();
-                    int recipePic = 2131230935;
-                    String recipeName = recipeSnapshot.child("recipeName").getValue().toString();
-                    int foodFave = Integer.valueOf(recipeSnapshot.child("faveCount").getValue().toString());
-                    double rating = Double.valueOf(recipeSnapshot.child("rating").getValue().toString());
-                    String contributorId = recipeSnapshot.child("contributorId").getValue().toString();
-                    String desc = recipeSnapshot.child("description").getValue().toString();
-                    int reviewCount = Integer.valueOf(recipeSnapshot.child("reviewCount").getValue().toString());
-                    int cookingTime = Integer.valueOf(recipeSnapshot.child("cookingTime").getValue().toString());
-                    int prepTime = Integer.valueOf(recipeSnapshot.child("prepTime").getValue().toString());
-                    int servings = Integer.valueOf(recipeSnapshot.child("cookingTime").getValue().toString());
-                    String category = recipeSnapshot.child("category").getValue().toString();
-                    String difficulty = recipeSnapshot.child("difficulty").getValue().toString();
-                    ArrayList<String> ingredientList = new ArrayList();
-                    ArrayList<Ingredient> ingredientDetailsList = new ArrayList<>();
-                    ingredientList = recipeSnapshot.child("ingredientsList").getValue(new GenericTypeIndicator<ArrayList<String>>(){});
-
-                    for (int i = 0; i < ingredientList.size(); i++) {
-                        for (int j = 0; j < allIngredientList.size(); j++){
-                            if (ingredientList.get(i).equalsIgnoreCase(allIngredientList.get(j).getId())){
-                                ingredientDetailsList.add(allIngredientList.get(j));
-                            }
-                        }
-                    }
-                    Log.d("INGRNAME", ingredientDetailsList.get(0).getIngredientName());
-
-                    tempRecipe[0] = new Recipe(id, recipePic, recipeName, foodFave, rating, contributorId, desc, reviewCount, cookingTime, prepTime, servings, category, difficulty, ingredientList, ingredientDetailsList);
-                    recipeList.add(tempRecipe[0]);
-                    Log.d("RECIPE LIST SIZE", String.valueOf(recipeList.size()));
-                    Log.d("INGREDIENTS", String.valueOf(tempRecipe[0].getIngredientsList()));
-
-                    Log.d("getKey", id);
-                    Log.d("name", recipeSnapshot.child("recipeName").getValue().toString());
-                    Log.d("recipe", String.valueOf(tempRecipe[0]));
-                    Log.d("test name", tempRecipe[0].getRecipeName());
-                    Log.d("test id", tempRecipe[0].getId());
-                    ctr += 1;
-                    Log.d("CTR", String.valueOf(ctr));
-                }
-
-                initPopularRecyclerView();
-                initRecentFeed();
-
-            }
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
-    }
     private void initPopularRecyclerView(){
-
-        //DataHelper helper = new DataHelper();
-        this.popularRecipeList = this.recipeList;
+        this.popularRecipeList = DataHelper.sortedRecipes;
         Log.d("SIZE FINAL", String.valueOf(this.popularRecipeList.size()));
         this.rvPopular = findViewById(R.id.rv_home_popular);
 
@@ -235,8 +108,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void initRecentFeed(){
-
-        this.recentRecipeList = this.recipeList;
+        this.recentRecipeList = DataHelper.allRecipes;
         this.rvRecent = findViewById(R.id.rv_home_recent);
 
         this.recentManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
