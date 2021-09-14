907fe1d7d8433e6132081340c258ecb85cee80dc
tanal <tanal@DESKTOP-G2ICFLV.local>
2021-09-14 16:16:55 +0800

    add storage to datahelper

--- app/src/main/java/com/mobdeve/s14/group4/DataHelper.java
+++ app/src/main/java/com/mobdeve/s14/group4/DataHelper.java
@@ -1,55 +1,61 @@
 package com.mobdeve.s14.group4;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class DataHelper {
+    public static User user;
+    public static ArrayList<Recipe> allRecipes;
+    public static ArrayList<Recipe> sortedRecipes;
 
-    public ArrayList<Recipe> initFood(){
-        ArrayList<Recipe> foodList = new ArrayList<>();
+    public static void loadUser(String uid){
+        new UserDatabase().getUser(uid, new CallbackListener() {
+            @Override
+            public void onSuccess(Object o) {
+                setGlobalUser((User) o);
+            }
 
-        int[] foodPics = {
-                R.drawable.popular1,
-                R.drawable.popular2,
-                R.drawable.popular3
-        };
-        String[] foodName ={
-                "Egg Toast",
-                "Fruity French Toast",
-                "Meatballs"
-        };
-        Integer[] foodFave = {
-                23,
-                35,
-                123
-        };
-
-        Double[] foodRating = {
-                4.3,
-                5.0,
-                3.8
-        };
-
-        String[] contributorId = {
-                "1",
-                "2",
-                "3"
-        };
-        String[] description = {
-                "This Egg Toast is so easy to make, filling, and so incredibly tasty, that it is going to make your morning better. Guaranteed. ",
-                "With so many toppings and breads there is an endless variety of French toast you can make. Cinnamon French toast, Brioche French toast, stuffed French toast and even a French toast that tastes like a Starbucks caramel macchiato. If you are feeling adventurous, you can even make French toast in a mug! ",
-                "I never knew how to make good meatballs until I found this recipe. I normally make mine with just ground beef and they still taste great. I've used the combination of pork, beef and veal and they are equally good. Definitely use fresh bread crumbs and freshly grate your cheese instead of using the canned variety...it really does make a difference."
-        };
-        Integer[] reviewCount = {
-                123,
-                456,
-                4500
-        };
-
-        for(int i = 0; i < 3; i++){
-            foodList.add(new Recipe(foodPics[i], foodName[i], foodFave[i], foodRating[i], contributorId[i], description[i], reviewCount[i], 0, 0, 0, "Main", "Easy"));
-        }
-
-        return foodList;
+            @Override
+            public void onFailure() {
+                Log.d("MISSING", "User" + uid + " not found");
+            }
+        });
     }
 
+    public static void loadRecipes(){
+        new RecipeDatabase().getAllRecipes(new CallbackListener() {
+            @Override
+            public void onSuccess(Object o) {
+                allRecipes = (ArrayList<Recipe>) o;
+                sortedRecipes = (ArrayList<Recipe>) allRecipes.clone();
+                Collections.sort(sortedRecipes, new Comparator<Recipe>() {
+                    @Override
+                    public int compare(Recipe o1, Recipe o2) {
+                        double o1Rating = o1.getRating();
+                        double o2Rating = o2.getRating();
+                        if (o1Rating > o2Rating){
+                            return 1;
+                        }
+                        else if (o1Rating < o2Rating){
+                            return -1;
+                        }
+
+                        return 0;
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure() {
+                Log.d("FAILURE", "Failed to getRecipes");
+            }
+        });
+    }
+
+    public static void setGlobalUser(User u){
+        user = u;
+    }
 }
