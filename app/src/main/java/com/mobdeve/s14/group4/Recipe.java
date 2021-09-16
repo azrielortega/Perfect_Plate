bfb9481f5a4785347137ebf47dd6c9ec78706a95
tanal <tanal@DESKTOP-G2ICFLV.local>
2021-09-16 19:50:43 +0800

    refactor get pic from recipe

--- app/src/main/java/com/mobdeve/s14/group4/Recipe.java
+++ app/src/main/java/com/mobdeve/s14/group4/Recipe.java
@@ -49,13 +49,12 @@ public class Recipe extends FirebaseRecipe
                   String category, String difficulty, ArrayList<String> ingredientList, ArrayList<String> stepsList, UploadImage image){
         super(id, recipePic, recipeName, foodFave, rating, contributorId, desc, reviewCount, cookingTime, prepTime, servings, category, difficulty, ingredientList, stepsList, image);
     }
-
-    public int getContributorPic(){
+    
+    public UploadImage getContributorPic(){
         if (contributor != null){
-            return contributor.getUserPic();
+            return contributor.getProfile_Image();
         }
-
-        return R.drawable.person_gray;
+        return null;
     }
 
     public String getContributorName(){
