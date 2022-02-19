d59d768a387478f24394de6918dc939bbe2debe1
littleSparrow7 <40954960+littleSparrow7@users.noreply.github.com>
2022-02-19 22:47:17 +0800

    add Address class

---
+++ app/src/main/java/com/mobdeve/s14/group4/Address.java
@@ -0,0 +1,47 @@
+package com.mobdeve.s14.group4;
+
+public class Address {
+    private String street;
+    private String state;
+    private String city;
+    private String postalCode;
+
+    public Address(String street, String city, String state, String postalCode){
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+    }
+
+    public String getStreet(){
+        return this.street;
+    }
+
+    public String getState(){
+        return this.state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
