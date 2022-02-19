2ca430e48111004b6c730638ba04452f9e819204
littleSparrow7 <40954960+littleSparrow7@users.noreply.github.com>
2022-02-19 23:01:36 +0800

    uncomment login

--- app/src/main/java/com/mobdeve/s14/group4/LoginActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/LoginActivity.java
@@ -13,25 +13,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-//
-//import com.google.android.gms.auth.api.signin.GoogleSignIn;
-//import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-//import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-//import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-//import com.google.android.gms.common.api.ApiException;
-//import com.google.android.gms.tasks.OnCompleteListener;
-//import com.google.android.gms.tasks.Task;
-//import com.google.firebase.auth.AuthCredential;
-//import com.google.firebase.auth.AuthResult;
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.FirebaseUser;
-//import com.google.firebase.auth.GoogleAuthProvider;
-//import com.google.firebase.database.ChildEventListener;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -39,15 +28,14 @@ public class LoginActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 1;
 
     private Button btnLogin;
-    private Button btnGoogleLogin;
 
     private EditText etEmail;
     private EditText etPassword;
 
-    //private FirebaseAuth mAuth;
-    //private FirebaseDatabase mDatabase;
-    //private DatabaseReference databaseReference;
-    //private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     private ProgressBar pbLogin;
 
@@ -57,37 +45,36 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         this.initComponents();
-        //this.initFirebase();
+        this.initFirebase();
     }
 
-//    private void initFirebase(){
-//        //this.mDatabase = FirebaseDatabase.getInstance();
-//        this.mAuth = FirebaseAuth.getInstance();
-//        //this.databaseReference = mDatabase.getReference("users");
-//
-//    }
+    private void initFirebase(){
+        this.mDatabase = FirebaseDatabase.getInstance();
+        this.mAuth = FirebaseAuth.getInstance();
+        this.databaseReference = mDatabase.getReference("users");
+
+    }
 
     private void initComponents(){
         this.btnLogin = findViewById(R.id.login_btn_login);
-        //this.btnGoogleLogin = findViewById(R.id.login_btn_google);
         this.etEmail = findViewById(R.id.login_et_email);
         this.etPassword = findViewById(R.id.login_et_password);
         pbLogin = findViewById(R.id.login_pb);
 
 
 
-//        this.btnLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                pbLogin.setVisibility(View.VISIBLE);
-//                String email = etEmail.getText().toString().trim();
-//                String password = etPassword.getText().toString().trim();
-//
-//                if (!isEmpty(email, password)) {
-//                    signIn(email, password);
-//                }
-//            }
-//        });
+        this.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pbLogin.setVisibility(View.VISIBLE);
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if (!isEmpty(email, password)) {
+                    signIn(email, password);
+                }
+            }
+        });
     }
 
     private boolean isEmpty(String e, String p){
@@ -100,44 +87,44 @@ public class LoginActivity extends AppCompatActivity {
         return false;
     }
 
-//    private void signIn(String email, String password) {
-//        // this.pbLogin.setVisibility(View.VISIBLE); ADD PROGRESS BAR LATER
-//
-//        this.mAuth.signInWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                       @Override
-//                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                           successfulLogin();
-//                        } else {
-//                            failedLogin();
-//                        }
-//                    }
-//                });
-//    }
+    private void signIn(String email, String password) {
+        // this.pbLogin.setVisibility(View.VISIBLE); ADD PROGRESS BAR LATER
 
-//    private void successfulLogin(){
-//        authStateListener = new FirebaseAuth.AuthStateListener() {
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//                FirebaseUser user = firebaseAuth.getCurrentUser();
-//                pbLogin.setVisibility(View.GONE);
-//                if (user != null) {
-//                    // User is signed in
-//                    DataHelper.loadUser(user.getUid());
-//                    Log.d("SIGNED IN LOGGED IN", "onAuthStateChanged:signed_in:" + user.getUid());
-//
-//                    moveToHomeActivity();
-//                    finish();
-//                } else {
-//                    // User is signed out
-//                    Log.d("TAG", "onAuthStateChanged:signed_out");
-//                }
-//            }
-//        };
-//
-//        authStateListener.onAuthStateChanged(FirebaseAuth.getInstance());
-//    }
+        this.mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                       @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                           successfulLogin();
+                        } else {
+                            failedLogin();
+                        }
+                    }
+                });
+    }
+
+    private void successfulLogin(){
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                pbLogin.setVisibility(View.GONE);
+                if (user != null) {
+                    // User is signed in
+                    DataHelper.loadUser(user.getUid());
+                    Log.d("SIGNED IN LOGGED IN", "onAuthStateChanged:signed_in:" + user.getUid());
+
+                    moveToHomeActivity();
+                    finish();
+                } else {
+                    // User is signed out
+                    Log.d("TAG", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        authStateListener.onAuthStateChanged(FirebaseAuth.getInstance());
+    }
 
     private void failedLogin(){
         //this.btnNext.setVisibility(View.GONE);
