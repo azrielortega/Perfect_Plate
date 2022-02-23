a621a47c111888514cc313eeda9e117b1c27bd5f
littleSparrow7 <40954960+littleSparrow7@users.noreply.github.com>
2022-02-23 15:11:36 +0800

    remake datahelper

--- app/src/main/java/com/mobdeve/s14/group4/DataHelper.java
+++ app/src/main/java/com/mobdeve/s14/group4/DataHelper.java
@@ -11,38 +11,32 @@ import java.util.Comparator;
 public class DataHelper {
     public static User user;
 
-    public static ArrayList<User> allUsers;
-
     public static ArrayList<Book> allBooks;
-    public static int popularBooksCount;
-    public static ArrayList<Book> popularBooks;
-
-//    public static ArrayList<Ingredient> allIngredients;
-
-    public static ArrayList<Review> allReviews;
+    public static ArrayList<Order> allOrders; //only for admin; garbage data for regular user
 
     public static UserDatabase userDatabase;
     public static BookDatabase bookDatabase;
-//    public static IngredientDatabase ingredientDatabase;
-    public static ReviewDatabase reviewDatabase;
+    public static OrderDatabase orderDatabase;
 
     public static void initUser(String uid){
         loadUser(uid);
     }
 
     public static void initDatabase(){
-        allUsers = new ArrayList<User>();
         allBooks = new ArrayList<Book>();
-        popularBooks = new ArrayList<Book>();
-        allReviews = new ArrayList<Review>();
-
+        allOrders = new ArrayList<Order>();
 
         userDatabase = new UserDatabase();
         bookDatabase = new BookDatabase();
-//        ingredientDatabase = new IngredientDatabase();
-        reviewDatabase = new ReviewDatabase();
+        orderDatabase = new OrderDatabase();
 
-        final CallbackListener booksListener = new CallbackListener() {
+        refreshBooks();
+        if (user.checkAdmin())
+            refreshOrders();
+    }
+
+    public static void refreshBooks(){
+        initBooks(new CallbackListener() {
             @Override
             public void onSuccess(Object o) {
 
@@ -52,31 +46,52 @@ public class DataHelper {
             public void onFailure() {
 
             }
-        };
-
-        refreshDatabase(booksListener);
+        });
     }
 
-    public static void refreshDatabase(final CallbackListener recipesListener){
-//        initAllIngredients();
-        initBooks(recipesListener);
-        initAllReviews();
-        initAllUsers();
-    }
-
-    public static void asyncRefreshDatabase(Context context, final CallbackListener booksListener){
+    public static void asyncRefreshBooks(Context context, final CallbackListener booksListener){
         Handler handler = new Handler(context.getMainLooper());
 
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                DataHelper.refreshDatabase(booksListener);
+                DataHelper.initBooks(booksListener);
             }
         };
 
         handler.post(runnable);
     }
 
+    public static void refreshOrders(){
+        initOrders(new CallbackListener() {
+            @Override
+            public void onSuccess(Object o) {
+
+            }
+
+            @Override
+            public void onFailure() {
+
+            }
+        });
+    }
+
+    public static void asyncRefreshOrders(Context context, final CallbackListener ordersListener){
+        Handler handler = new Handler(context.getMainLooper());
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                DataHelper.initOrders(ordersListener);
+            }
+        };
+
+        handler.post(runnable);
+    }
+
+    //
+    // USER
+    //
     public static void loadUser(String uid){
         userDatabase.getUser(uid, new CallbackListener() {
             @Override
@@ -91,104 +106,74 @@ public class DataHelper {
         });
     }
 
-    public static void initAllUsers(){
-        userDatabase.getAllUsers(new CallbackListener() {
-            @Override
-            public void onSuccess(Object o) {
-                allUsers = (ArrayList<User>) o;
-            }
-
-            @Override
-            public void onFailure() {
-                Log.d("FAILURE", "Failed to get users");
-            }
-        });
+    public static void setGlobalUser(User u){
+        user = u;
     }
 
+    //
+    // BOOKS
+    //
     public static void initBooks(final CallbackListener listener){
         bookDatabase.getAllBooks(new CallbackListener() {
             @Override
             public void onSuccess(Object o) {
                 allBooks = (ArrayList<Book>) o;
-                updatePopularity();
                 listener.onSuccess(allBooks);
             }
 
             @Override
             public void onFailure() {
                 listener.onFailure();
-                Log.d("FAILURE", "Failed to get recipes");
+                Log.d("FAILURE", "Failed to get books");
             }
         });
-    }
-
-//    public static void initAllIngredients(){
-//        ingredientDatabase.getAllIngredients(new CallbackListener() {
-//            @Override
-//            public void onSuccess(Object o) {
-//                allIngredients = (ArrayList<Ingredient>) o;
-//                Log.d("ALLING", String.valueOf(allIngredients.size()));
-//            }
-//
-//            @Override
-//            public void onFailure() {
-//                Log.d("FAILURE", "Failed to get ingredients");
-//            }
-//        });
-//    }
-
-    public static void initAllReviews(){
-        reviewDatabase.getAllReviews(new CallbackListener() {
-            @Override
-            public void onSuccess(Object o) {
-                allReviews = (ArrayList<Review>) o;
-                Log.d("ALLREVIEWS", String.valueOf(allReviews.size()));
-            }
-
-            @Override
-            public void onFailure() {
-                Log.d("FAILURE", "Failed to get reviews");
-            }
-        });
-
     }
 
     public static void addBook(Book book){
         allBooks.add(book);
+    }
 
-        if (popularBooksCount < 10){
-            popularBooksCount++;
-            popularBooks.add(book);
+    public static void updateBook(Book book){
+        for (Book b : allBooks){
+            if (b.getId().equals(book.getId())){
+                b = book;
+            }
         }
     }
 
-    public static void updatePopularity(){
-        ArrayList<Book> newPopular = (ArrayList<Book>) allBooks.clone();
+    public static void deleteBook(String id){
+        ArrayList<Book> forDeletion = new ArrayList<>();
 
-        Collections.sort(newPopular, new Comparator<Book>() {
-            @Override
-            public int compare(Book o1, Book o2) {
-                int o1Fave = o1.getFaveCount();
-                int o2Fave = o2.getFaveCount();
+        for (Book b : allBooks){
+            if (b.getId().equals(id))
+                forDeletion.add(b);
+        }
 
-                if (o1Fave > o2Fave){
-                    return -1;
-                }
-                else if (o1Fave < o2Fave){
-                    return 1;
-                }
-
-                return 0;
-            }
-        });
-
-        int bookCount = allBooks.size();
-        popularBooksCount = (bookCount > 10)? 10 : bookCount;
-
-        popularBooks = new ArrayList<>(newPopular.subList(0, popularBooksCount));
+        for (Book b: forDeletion) {
+            allBooks.remove(b);
+        }
     }
 
-    public static void setGlobalUser(User u){
-        user = u;
+    //
+    // ORDERS
+    //
+    public static void initOrders(final CallbackListener listener){
+        orderDatabase.getAllOrders(new CallbackListener() {
+            @Override
+            public void onSuccess(Object o) {
+                allOrders = (ArrayList<Order>) o;
+                listener.onSuccess(allOrders);
+            }
+
+            @Override
+            public void onFailure() {
+                listener.onFailure();
+                Log.d("FAILURE", "Failed to get books");
+            }
+        });
+    }
+
+    public static void addOrder(Order order){
+        allOrders.add(order);
     }
 }
