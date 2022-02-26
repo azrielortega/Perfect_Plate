1322fa8a93d3c3a38a4c7b7a490b4ba1bfa01efb
Azriel <44496680+azrielortega@users.noreply.github.com>
2022-02-26 18:46:19 +0800

    added redirect to signin in signup page 1

--- app/src/main/java/com/mobdeve/s14/group4/SignUpActivity1.java
+++ app/src/main/java/com/mobdeve/s14/group4/SignUpActivity1.java
@@ -58,7 +58,7 @@ public class SignUpActivity1 extends AppCompatActivity {
         tvSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                finish();
             }
         });
 
