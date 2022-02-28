05584e5ebfbbb4e06fc849e69691d9de14dc8489
littleSparrow7 <40954960+littleSparrow7@users.noreply.github.com>
2022-02-28 19:39:07 +0800

    connect views

--- app/src/main/java/com/mobdeve/s14/group4/AddToCartActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/AddToCartActivity.java
@@ -1,9 +1,11 @@
 package com.mobdeve.s14.group4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -12,6 +14,7 @@ import android.widget.TextView;
 public class AddToCartActivity extends AppCompatActivity {
     private TextView tvTotal;
     private ImageButton ibBack;
+    private ConstraintLayout clCheckout;
 
     private RecyclerView rvCart;
     private RecyclerView.LayoutManager rvCartManager;
@@ -24,11 +27,20 @@ public class AddToCartActivity extends AppCompatActivity {
 
         this.tvTotal = findViewById(R.id.tv_cart_total);
         this.ibBack = findViewById(R.id.ib_cart_back);
+        this.clCheckout = findViewById(R.id.cl_cart_checkout);
 
         this.ibBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        this.clCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AddToCartActivity.this, CheckoutActivity.class);
+                startActivity(i);
             }
         });
 
--- app/src/main/java/com/mobdeve/s14/group4/CheckoutActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/CheckoutActivity.java
@@ -1,14 +1,54 @@
 package com.mobdeve.s14.group4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheckoutActivity extends AppCompatActivity {
+    public TextView tvCustomer;
+    public TextView tvAddressStreet;
+    public TextView tvAddressArea;
+    public TextView tvTotal;
+
+    public RecyclerView rvOrder;
+    public RecyclerView.LayoutManager rvOrderManager;
+    public CheckoutAdapter checkoutAdapter;
+
+    public Button btnPlaceOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        this.tvCustomer = findViewById(R.id.tv_checkout_name);
+        this.tvAddressStreet = findViewById(R.id.tv_checkout_street_address);
+        this.tvAddressArea = findViewById(R.id.tv_city_province_postal);
+        this.tvTotal = findViewById(R.id.tv_checkout_total);
+
+        this.btnPlaceOrder = findViewById(R.id.btn_checkout_place_order);
+
+        this.rvOrder = findViewById(R.id.rv_checkout_summary);
+        this.rvOrderManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        this.checkoutAdapter = new CheckoutAdapter(DataHelper.user.getCart().getOrderDetails());
+
+        this.rvOrder.setLayoutManager(this.rvOrderManager);
+        this.rvOrder.setAdapter(this.checkoutAdapter);
+
+        setDetails();
+    }
+
+    private void setDetails(){
+        this.tvCustomer.setText(DataHelper.user.getFullName());
+
+        Address address = DataHelper.user.getAddress();
+        this.tvAddressStreet.setText(address.getStreet());
+        this.tvAddressArea.setText(address.getCity() + ", " + address.getState() + " " + address.getPostalCode());
+
+        this.tvTotal.setText(String.format("%.2f", DataHelper.user.getCart().getTotal()));
     }
 }
