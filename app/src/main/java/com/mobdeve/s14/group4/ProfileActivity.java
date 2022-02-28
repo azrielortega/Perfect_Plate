d9bf865bb205b54a8a4d3df60d5d44c9770d20e5
Azriel <44496680+azrielortega@users.noreply.github.com>
2022-02-28 16:46:53 +0800

    connected profile

--- app/src/main/java/com/mobdeve/s14/group4/ProfileActivity.java
+++ app/src/main/java/com/mobdeve/s14/group4/ProfileActivity.java
@@ -3,12 +3,35 @@ package com.mobdeve.s14.group4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView tvName;
+    private TextView tvContactNo;
+    private TextView tvStreet;
+    private TextView tvAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        tvName = findViewById(R.id.tv_profile_name);
+        tvContactNo = findViewById(R.id.tv_profile_number);
+        tvStreet = findViewById(R.id.tv_profile_street);
+        tvAddress = findViewById(R.id.tv_profile_city_province_postal);
+
+        User user = DataHelper.user;
+        String address = user.getAddress().getCity() + ", " + user.getAddress().getState() + ", " + user.getAddress().getPostalCode();
+
+        tvName.setText(user.getFullName());
+        tvContactNo.setText(user.getContactNo());
+        tvStreet.setText(user.getAddress().getStreet());
+        tvAddress.setText(address);
     }
 }
