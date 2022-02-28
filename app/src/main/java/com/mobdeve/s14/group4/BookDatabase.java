29079f6443aeb6654a05e4c7e9376c9379069eae
littleSparrow7 <40954960+littleSparrow7@users.noreply.github.com>
2022-02-28 14:07:29 +0800

    add transactions for increasing and decreasing stock

--- app/src/main/java/com/mobdeve/s14/group4/BookDatabase.java
+++ app/src/main/java/com/mobdeve/s14/group4/BookDatabase.java
@@ -3,12 +3,15 @@ package com.mobdeve.s14.group4;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
@@ -97,18 +100,73 @@ public class BookDatabase {
         return key;
     }
 
-    public void updateBook(Book book){
-        Log.d("myTag", "Update Book");
-
-        this.databaseReference.child(book.getId()).setValue(book);
-        DataHelper.updateBook(book);
-    }
-
     /**
      * Delete book from books database.
      * */
     public void deleteBook(String id){
         this.databaseReference.child(id).setValue(null);
         DataHelper.deleteBook(id);
+    }
+
+    /**
+    * for increasing stock
+     * @param id String id of book
+     * @param change int number to increase
+     * @param callbackListener indicates whether succeeded or failed
+    * */
+    public void increaseStock(String id, int change, CallbackListener callbackListener){
+        updateStock(id, change, true, callbackListener);
+    }
+
+    /**
+     * for decreasing stock
+     * @param id String id of book
+     * @param change int number to decrease
+     * @param callbackListener indicates whether succeeded or failed
+     * */
+    public void decreaseStock(String id, int change, CallbackListener callbackListener){
+        updateStock(id, change, false, callbackListener);
+    }
+
+    /**
+     * updates stock
+     * */
+    private void updateStock(String id, int change, boolean wasIncreased, CallbackListener callback){
+        this.databaseReference.child(id).child("stock").runTransaction(new Transaction.Handler() {
+            @NonNull
+            @NotNull
+            @Override
+            public Transaction.Result doTransaction(@NonNull @NotNull MutableData currentData) {
+                if (currentData.getValue() != null){
+                    int stock;
+
+                    if (wasIncreased){
+                        stock = currentData.getValue(int.class) + change;
+                    }
+                    else{
+                        stock = currentData.getValue(int.class) - change;
+
+                        if (stock < 0){
+                            return Transaction.abort();
+                        }
+
+                    }
+
+                    currentData.setValue(stock);
+                }
+
+                return Transaction.success(currentData);
+            }
+
+            @Override
+            public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, boolean committed, @Nullable @org.jetbrains.annotations.Nullable DataSnapshot currentData) {
+                if (committed){
+                    callback.onSuccess(currentData);
+                }
+                else{
+                    callback.onFailure();
+                }
+            }
+        });
     }
 }
